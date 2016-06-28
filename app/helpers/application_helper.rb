ac043ec1e2b6eddd9fdb487a9c3f03af1f69f1cd
Evans Musomi <evanso.dev@gmail.com>
2016-06-28 09:23:08 +0300

    Finalises Transactions List UI and Navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+	# Make date to more user friendly format
+	def human_date(date)
+		date.strftime("%B %-d, %Y")
+	end
+
+	# Make time more user friendly format
+	def human_time(date)
+		date.strftime("on %d/%m/%y at %k:%I %p")
+	end
+
+	# Escape HTML
+	def innocent(html)
+		html.html_safe
+	end
 end

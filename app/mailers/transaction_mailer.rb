df77442b027fc69ba46f0c8772ef7625009c1076
Evans Musomi <evanso.dev@gmail.com>
2016-06-28 14:01:05 +0300

    Refactors devise emails to address by name

--- app/mailers/transaction_mailer.rb
+++ app/mailers/transaction_mailer.rb
@@ -2,6 +2,6 @@ class TransactionMailer < ApplicationMailer
 	def received_money_email(transaction)
 		@transaction = transaction
 		email_with_name = "#{@transaction.recipient.name} <#{@transaction.recipient.email}>"
-		mail(to: email_with_name, subject: "#{@transaction.sender.name} sent you money on WPESA")
+		mail(to: email_with_name, subject: "You're KES #{@transaction.amount} richer on WPESA")
 	end
 end

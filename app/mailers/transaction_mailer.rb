4d5bda35dc419e62e436b119f46a45495d44ebbc
Evans Musomi <evanso.dev@gmail.com>
2016-06-28 12:43:15 +0300

    Adds email templates for 'money received' email notification

--- app/mailers/transaction_mailer.rb
+++ app/mailers/transaction_mailer.rb
@@ -1,2 +1,6 @@
 class TransactionMailer < ApplicationMailer
+	def received_money_email(transaction)
+		@transaction = transaction
+		mail(to: @transaction.recipient.email, subject: "#{@transaction.sender.name} sent you money on WPESA")
+	end
 end

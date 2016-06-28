4f81aab9d00677d51384f7058e7c4aee95e0ed08
Evans Musomi <evanso.dev@gmail.com>
2016-06-28 11:15:57 +0300

    Adds active page detection to navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,13 @@ module ApplicationHelper
 	def innocent(html)
 		html.html_safe
 	end
+
+	# Set active nav based on current page
+	def nav_link(link_text, link_path)
+		class_name = current_page?(link_path) ? 'active' : ''
+
+		content_tag(:li, class: class_name) do
+			link_to link_text, link_path
+		end
+	end
 end
